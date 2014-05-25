c25176f4db900835be2b5242b89e04e0905e02d4
Vladimir-csp <vladimir-csp@yandex.ru>
2014-05-25 13:14:23 +0400

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+[ "$UID" != "0" ] && echo "Not root!" && exit 1
+mkdir -p /opt/mass-print && cp --preserve=timestamps,mode -r mass-print locale /opt/mass-print/ && ln -s /opt/mass-print/mass-print /usr/local/bin/mass-print
